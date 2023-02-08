f9a020fefb1a5953adc56b27c3a5720e6b68198b
Tony <108546980+TonyKFC@users.noreply.github.com>
2023-02-08 23:43:36 +0800

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,35 @@
+fx_version 'bodacious'
+game 'gta5'
+
+lua54 'yes'
+client_scripts {
+    '@es_extended/locale.lua',
+    "config/shared.lua",
+    "client/VehicleManager_CL.lua",
+    "client/client.lua",
+    'locales/en.lua',
+    'locales/pl.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    "config/shared.lua",
+    "server/chatCommand.lua",
+    "server/server.lua",
+    'locales/en.lua',
+    'locales/pl.lua'
+}
+
+ui_page 'client/html/index.html'
+
+files {
+    'client/html/index.html',
+    'client/html/sounds/lock.ogg',
+    'client/html/sounds/unlock.ogg'
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'@es_extended/imports.lua'
+}
