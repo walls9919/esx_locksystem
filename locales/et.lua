6662d8e4a714175bcbd7ee60ad903bee62e04909
Walls <blumvaido@gmail.com>
2023-02-10 05:40:50 +0200

    Create et.lua
    Create estonian locales for the resource

---
+++ locales/et.lua
@@ -0,0 +1,17 @@
+Locales['et'] = {
+    ['vehicle_locked'] = 'Sõiduk lukustatud',
+    ['vehicle_unlocked']  = 'Sõiduk lukust lahti tehtud',
+    ['keys_not_inside'] = 'Te ei leidnud sõiduki võtmeid',
+    ['lock_cooldown'] = 'palun oota %s ,et uuesti proovida',
+    ['vehicle_not_owned'] = 'See sõiduk ei kuulu teile',
+    ['missing_argument_first'] = 'Esimene summa on tundmatu : /givekey <id> <plate>',
+    ['missing_argument_second'] = 'Teine summa on tundmatu  : /givekey <id> <plate>',
+    ['player_not_found'] = 'Mängijat ei leitud',
+    ['cannot_target_yourself'] = 'Sa ei saa võtmeid iseendale anda',
+    ['vehicle_not_exist'] = 'Märgitud numbrimärgiga sõidukit ei eksisteeri',
+    ['target_has_keys_sender'] = 'Isik, kellele proovisite võtmeid anda juba omab neid võtmeid',
+    ['target_has_keys_receiver'] = '%s pakkus teile sõiduki võtmeid, kuid teil on juba need olemas',
+    ['you_gave_keys'] = "Sa andsid auto võtmed %s %s le",
+    ['you_received_keys'] = "Saite sõiduki võtmed %s %s lt",
+    ['could_not_find_plate'] = "Numbrimärki ei leitud!"
+}
